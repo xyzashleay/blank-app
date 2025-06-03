6cf7bfc4e750c3f5d7375dfa3a3c6f1f42b2cd3f
xyzashleay <163875378+xyzashleay@users.noreply.github.com>
2025-06-04 01:27:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,7 +114,7 @@ def welcome_page():
         unsafe_allow_html=True
     )
 
-    st.markdown("<h1 style='text-align: center; color: white; font-size: 50px;'>DATABASE ALUMNI S1</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: white; font-size: 50px;'>DATABASE ALUMNI S1</h2>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: white; font-size: 50px;'>Departemen Matematika FMIPA UI</h2>", unsafe_allow_html=True)
     st.markdown(
     """
